955904e230a45a11ccb4f731b166967e4790cf54
Atal <atal.bajracharya@gmailcom>
2017-08-01 09:20:08 -0400

    1st commit.Key expansion working for all 3 sims

---
+++ AES.srcs/sources_1/new/KeyIn.v
@@ -0,0 +1,52 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/15/2017 12:58:31 PM
+// Design Name: 
+// Module Name: HashIn
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module KeyIn(input wire clk,
+              input wire rst, 
+               input wire en,
+               input wire [7:0]addr, 
+               input wire [31:0]di,
+               output reg done,
+               output reg [255:0]KeyO
+               );
+parameter delay = 3'b0;
+parameter END_ADDR = 10;
+reg [31:0] RAM [7:0]; // 32bit RAM x 16 slots= 512 bit(64 Byte) RAM
+always @(posedge clk or negedge rst)begin
+  if(!rst) begin
+    done <= 0;
+    KeyO <= 0;
+  end 
+  else if(en)begin
+       if(addr < END_ADDR)begin 
+           RAM[addr-1] <= di;
+           if(addr == END_ADDR-1)begin 
+               KeyO <=  {RAM[0],RAM[1],RAM[2],RAM[3],RAM[4],RAM[5],RAM[6],RAM[7]};
+             //hashO <= {RAM[0],RAM[1],RAM[2],RAM[3],RAM[4]};
+             //msgO <= {RAM[5],RAM[6],RAM[7],RAM[8],RAM[9],RAM[10],RAM[11],RAM[12],RAM[13],RAM[14],RAM[15],RAM[16],RAM[17],RAM[18],RAM[19],RAM[20]};
+           end
+       end else if(addr >= END_ADDR)
+                   done <= #delay 1;
+   end
+end
+
+endmodule
---
+++ AES.srcs/sources_1/new/top.v
@@ -0,0 +1,51 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/28/2017 05:56:32 PM
+// Design Name: 
+// Module Name: top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+module top(input wire clk,
+           input wire rst,
+           input wire enKeyIn,
+           output done,
+           output [31:0]key           
+//           output [31:0]keyInBRAMOUT
+           //output [31:0]keyOut
+           );
+           
+wire Kdone;
+wire [255:0]keyOut;
+//wire [255:0]Kdo;
+//wire [511:0]Mdo;
+wire [31:0]keyInBRAMOUT;
+wire [7:0]keyAddr;
+//wire digestDone;
+//wire [159:0]digest;
+counter C0(clk,rst,enKeyIn,keyAddr);
+blk_mem_gen_0 B0(clk,enKeyIn,0,keyAddr,32'h0,keyInBRAMOUT);//BRAM for HashIn
+KeyIn K1(clk,rst,enKeyIn,keyAddr,keyInBRAMOUT,Kdone,keyOut);
+key_expansion KX1(clk,rst,Kdone,keyOut,done,key);
+//core_logic SHA1(clk,rst,Hdone,Hdo,Mdo,digestDone,digest,led0,ledout);//,led1);
+//uart_tx U1(clk,rst,digest,ready,TxD);
+
+endmodule
+
+
